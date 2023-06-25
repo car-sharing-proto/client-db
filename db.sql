dcc7caa237f4548133123b0cd840638b3c57d11f
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-06-25 16:36:54 +0300

    add cleint table

---
+++ db.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS Client;
+
+CREATE TABLE Client (
+    id INTEGER,
+    phone_number TEXT CHECK (
+        phone_number REGEXP
+        '^8[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$'),
+    accaunt_status TEXT CHECK (
+        accaunt_status IN (
+            'non-activated',
+            'activated',
+            'banned',
+            'closed'
+        )
+    ),
+    driving_style FLOAT CHECK (
+        driving_style >= 0 AND
+        driving_style <= 1
+    ),
+    rating FLOAT CHECK (
+        rating >= 0 AND
+        rating <= 1
+    ),
+    registration_date DATE NOT NULL,
+
+    PRIMARY KEY (id)
+);
