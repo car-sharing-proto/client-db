70d8ff258b49df96f4074cffb3d8c2219b235c75
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-06-25 16:55:10 +0300

    add bank card table

--- db.sql
+++ db.sql
@@ -1,10 +1,12 @@
+DROP TABLE IF EXISTS BankCard;
 DROP TABLE IF EXISTS Client;
 
 CREATE TABLE Client (
     id INTEGER,
     phone_number TEXT CHECK (
         phone_number REGEXP
-        '^8\d{10}$'),
+        '^8\d{10}$'
+    ),
     accaunt_status TEXT CHECK (
         accaunt_status IN (
             'non-activated',
@@ -24,4 +26,21 @@ CREATE TABLE Client (
     registration_date DATE NOT NULL,
 
     PRIMARY KEY (id)
+);
+
+CREATE TABLE BankCard (
+    card_number TEXT CHECK (
+        card_number REGEXP
+        '\d{16}$'
+    ),
+    holder_name TEXT NOT NULL,
+    validity DATE NOT NULL,
+    code TEXT CHECK(
+        code REGEXP
+        '\d{3}$'
+    ),
+    client_id INTEGER,
+    
+    PRIMARY KEY (card_number),
+    FOREIGN KEY (client_id) REFERENCES Client (id) 
 );
